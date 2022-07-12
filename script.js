e809eb5d15a8c21d7741d32077cfc18854fcc251
Carlos Molina <carlos.molina.artigot@gmail.com>
2022-07-12 15:26:57 +0200

    Configuración primeras funciones en JS

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Desafio Arrays - Ejemplo de Gestión de Personas en una empresa
 
+//Construimos la clase para las Personas
+
 class Personal {
-    constructor(nombre, apellido, edad, posicion, vacaciones, sueldo, equipo) {
+    constructor(id, nombre, apellido, edad, posicion, vacaciones, sueldo, equipo) {
+        this.id = id
         this.nombre = nombre
         this.apellido = apellido
         this.edad = edad
@@ -17,61 +20,245 @@ class Personal {
         this.vacaciones = this.vacaciones -= diasVacaciones
     }
 }
-trabajador7 = new Personal ("Carlos", "Godoy", 32, "Project Manager", 19, 38000, [])
-trabajador6 = new Personal ("Reka", "Barath", 35, "Operations Manager", 36, 40000, [])
-trabajador5 = new Personal ("Sofia", "Yonico", 31, "Marketing Sales", 14, 38000, [])
-trabajador4 = new Personal ("Fiorella", "Adamo", 28, "Marketing Manager", 22, 35000, [])
-trabajador3 = new Personal ("Pablo", "Fiestas", 40, "VP Operaciones", 15, 50000, [trabajador6, trabajador7])
-trabajador2 = new Personal ("Carlos", "Molina", 45, "VP Sales", 18, 50000, [trabajador4, trabajador5])
-trabajador1 = new Personal ("Raúl", "De Pablos", 48, "CEO", "22", "70000", [trabajador2, trabajador3])
+
+//Agregamos los objetos con la plantilla actual
+
+trabajador7 = new Personal (6, "Carlos", "Godoy", 32, "Project Manager", 19, 38000, [])
+trabajador6 = new Personal (5, "Reka", "Barath", 35, "Operations Manager", 36, 40000, [])
+trabajador5 = new Personal (4, "Sofia", "Yonico", 31, "Marketing Sales", 14, 38000, [])
+trabajador4 = new Personal (3, "Fiorella", "Adamo", 28, "Marketing Manager", 22, 35000, [])
+trabajador3 = new Personal (2, "Pablo", "Fiestas", 40, "VP Operaciones", 15, 50000, [trabajador6, trabajador7])
+trabajador2 = new Personal (1, "Carlos", "Molina", 45, "VP Sales", 18, 50000, [trabajador4, trabajador5])
+trabajador1 = new Personal (0, "Raúl", "De Pablos", 48, "CEO", "22", "70000", [trabajador2, trabajador3])
+
+let trabajador8
+let empleadoBaja
+let otraGestion
+let seleccionaGestion 
+
+//Creamos un Array con cada Departamento
 
 const directores = [trabajador1, trabajador2, trabajador3]
 const marketingyventas = [trabajador4, trabajador5]
 const operaciones = [trabajador6, trabajador7]
 
-const totalEmpleados = (directores.concat(marketingyventas.concat(operaciones)))
-
-//Calculamos cuántos empleados en total tiene la empresa actualmente (ejemplo de uso de Length)
-console.log(`La empresa tiene actualmente" ${totalEmpleados.length} "empleados`)
-
-//Contratamos a un nuevo empleado y se incluye en la plantilla
-
-trabajador8 = new Personal (("Diana", "Mottura", 25, "IT Manager", 40000, []))
-
-//Se actualiza el objetivo trabajador3 con la incorporación al equipo
-trabajador3 = new Personal ("Pablo", "Fiestas", 40, "VP Operaciones", 15, 50000, [trabajador6, trabajador7, trabajador8])
-
-//Se agrega el nuevo trabajador al Array de Operaciones y se concatena de nuevo el total de empleados en una nueva constante
-operaciones.push (trabajador8) 
-const totalEmpleados2 = (directores.concat(marketingyventas.concat(operaciones)))
-console.log(`La empresa tiene actualmente" ${totalEmpleados2.length} "empleados`)
-
-console.log(totalEmpleados2)
+//Función Concat para agrupar el total de empleados en un único Array
 
 
-//El trabajador Carlos Godoy deja la compañía
+//Creamos un Array con las Gestiones a Realizar en la aplicación de RR.HH.
 
-console.log(operaciones.indexOf(trabajador7))
-
-const bajaTrabajador = operaciones.splice(1,1)
-console.log(`El trabajador ${trabajador7.nombre} ${trabajador7.apellido} se ha dado de baja de la compañía`)
-console.log(bajaTrabajador)
-trabajador3 = new Personal ("Pablo", "Fiestas", 40, "VP Operaciones", 15, 50000, [trabajador6, trabajador8])
+let gestiones = ["Alta nuevo empleado", "Baja de empleado", "Incrementar sueldo (No disponble)", "Registrar vacaciones (No disponible)", "Mostrar el Total de Empleados"]
 
 
-const totalEmpleados3 = (directores.concat(marketingyventas.concat(operaciones)))
-console.log(`La empresa tiene actualmente" ${totalEmpleados3.length} "empleados`)
-console.log(totalEmpleados3)
 
-//Todos los empleados se toman 5 días de vacaciones
+//Función para Seleccionar que tipo de gestión realizar
 
-let diasVacaciones = 5
-
-trabajador1.tomarVacaciones(diasVacaciones)
-trabajador4.tomarVacaciones(diasVacaciones)
-trabajador5.tomarVacaciones(diasVacaciones)
-
-console.log(`Se han actualizado las vacaciones de ${trabajador4.nombre}, ${trabajador1.nombre} y  ${trabajador5.nombre} que se han tomado ${diasVacaciones} días de vacaciones`)
-console.log(operaciones, marketingyventas, directores)
+function seleccionGestion () {
+    seleccionaGestion = prompt("Selecciona la gestión a realizar:\n1. " + gestiones[0] + "\n2." + gestiones[1] + "\n3." + gestiones[2] + "\n4." + gestiones[3] + "\n5." + gestiones[4] + "\nSelecciona un número del 1 al 5 con la gestión a realizar")
+}
 
 
+//Función para calcular y mostrar el Total de Empleados
+
+function calcularEmpleados () {
+    const totalEmpleados = directores.concat(marketingyventas.concat(operaciones))
+    console.log(`La empresa tiene actualmente" ${totalEmpleados.length} "empleados`)
+    totalEmpleados.forEach(empleado => {console.log(empleado.nombre, empleado.apellido)});
+}
+
+
+//Función Dar de Alta a un nuevo emplead@ en el equipo de Operaciones
+
+function altaEmpleado () {
+    trabajador8 = new Personal (7, prompt("Ingresa el nombre de la persona"), prompt("Ingresa el apellido"), prompt("Ingresa su edad"), prompt("Ingresa su posición"), 22, prompt("Ingresa su sueldo"))    
+}
+
+
+//Función para dar de baja un empleado (sólo funciona para el listado inicial de empleados)
+
+function seleccionarEmpleado () {
+
+    const totalEmpleados = directores.concat(marketingyventas.concat(operaciones))
+    empleado = prompt("Selecciona el empleado sobre el que quieres actuar:\n1. " + totalEmpleados[0].nombre + " " + totalEmpleados[0].apellido + "\n2." + totalEmpleados[1].nombre + " " + totalEmpleados[1].apellido + "\n3." + totalEmpleados[2].nombre + " " + totalEmpleados[2].apellido + "\n4." + totalEmpleados[3].nombre + " " + totalEmpleados[3].apellido + "\n5." + totalEmpleados[4].nombre + " " + totalEmpleados[4].apellido + "\n6." + totalEmpleados[5].nombre + " " + totalEmpleados[5].apellido + "\n7." + totalEmpleados[6].nombre + " " + totalEmpleados[6].apellido + "\nSelecciona un número del 1 al 7 para seleccionar el empleado sobre el que realizar la Gestión")
+}
+
+//Función realizar gestión adicional 
+/*
+function gestionAdicional () {
+    let otraGestion = prompt("¿Deseas realizar otra gestión? (Si o No)")
+    otraGestion.toLowerCase()
+
+    if (otraGestion = "si") {
+        alert ("Perfecto. Continua para seleccionar el tipo de gestión que quieres realizar")
+    } else alert ("Muchas gracias por tu tiempo, vuelve cuando quieras")
+}
+*/
+
+//Iniciamos el Programa de Gestión
+
+    usuario = prompt("Bienvenid@, introduce tu nombre de usuario")
+    alert (`Gracias ${usuario}. \nAhora vamos a iniciar la Gestión de Recursos Humanos`)
+
+    calcularEmpleados ()
+
+
+//Le pedimos al usuario que seleccione que Gestión quiere realizar
+
+do {
+
+    do {
+        seleccionGestion ()
+        
+        if(seleccionaGestion >0 && seleccionaGestion <= 5) 
+        {
+            switch (seleccionaGestion) {
+                case "1": 
+                    gestion = "Alta nuevo empleado"
+                    break
+                case "2": 
+                    gestion = "Baja de empleado"
+                    break
+                case "3": 
+                    gestion = "Incrementar sueldo"
+                    break
+                case "4": 
+                    gestion = "Registrar vacaciones"
+                    break
+                case "5":
+                    gestion = "Total empleados"
+            }
+        } else alert("Debes ingresar un valor en formato número entre el 1 y el 5")
+
+    } while (isNaN(seleccionaGestion) || seleccionaGestion <0 || seleccionaGestion >5) 
+    console.log(gestion)
+
+//Ejecutamos función de Dar Alta
+
+    if (gestion == "Alta nuevo empleado") { 
+        
+        altaEmpleado ()
+
+        //Se actualiza el objeto trabajador3 con la incorporación al equipo
+        trabajador3 = new Personal ("Pablo", "Fiestas", 40, "VP Operaciones", 15, 50000, [trabajador6, trabajador7, trabajador8])
+
+        //Se agrega el nuevo trabajador al Array de Operaciones y se concatena de nuevo el total de empleados en una nueva constante
+        operaciones.push (trabajador8)
+        
+        calcularEmpleados()
+    }  
+
+
+//Ejecutamos la Función de Dar de Baja a un empleado
+
+    if (gestion == "Baja de empleado") {
+
+        do {
+            seleccionarEmpleado()
+        
+            if(empleado >0 && empleado <= 8) 
+            {
+                switch (empleado) {
+                    case "1": 
+                        posicionBaja = 0
+                        break
+                    case "2": 
+                        posicionBaja = 1
+                        break
+                    case "3": 
+                        posicionBaja = 2
+                        break
+                    case "4": 
+                        posicionBaja = 3
+                        break
+                    case "5": 
+                        posicionBaja = 4
+                        break
+                    case "6": 
+                        posicionBaja = 5
+                        break
+                    case "7": 
+                        posicionBaja = 6
+                        break
+                    case "8": 
+                        posicionBaja = 7
+                        break
+                }
+            } else alert("Debes ingresar un valor en formato número entre el 1 y el 8")
+
+        } while (isNaN(empleado)) 
+
+        const totalEmpleados = directores.concat(marketingyventas.concat(operaciones))
+        const nombreBaja = (totalEmpleados.splice(posicionBaja,1))
+
+        alert (`El trabajador ${nombreBaja[0].nombre} ${nombreBaja[0].apellido} se ha dado de baja de la compañía`)
+
+        console.log(`La empresa tiene actualmente" ${totalEmpleados.length} "empleados`)
+        totalEmpleados.forEach(empleado => {console.log(empleado.nombre, empleado.apellido)})
+    }
+
+
+//Ejecutamos la función de Registro de Vacaciones (Work in Progress)
+
+/*
+let diasVacaciones = prompt("Ingresa el número de días de Vacaciones disfrutadas")
+
+//Seleccionamos el empleado que se toma vacaciones
+
+    if (gestion == "Registrar vacaciones") {
+
+
+    do {
+        seleccionarEmpleado()
+        
+        if(empleado >0 && empleado <= 8) 
+        {
+            switch (empleado) {
+                case "1": 
+                    posicionVacaciones = 0
+                    break
+                case "2": 
+                    posicionVacaciones = 1
+                    break
+                case "3": 
+                    posicionVacaciones = 2
+                    break
+                case "4": 
+                    posicionVacaciones = 3
+                    break
+                case "5": 
+                    posicionVacaciones = 4
+                    break
+                case "6": 
+                    posicionVacaciones = 5
+                    break
+                case "7": 
+                    posicionVacaciones = 6
+                    break
+                case "8": 
+                    posicionVacaciones = 7
+                    break
+            }
+        } else alert("Debes ingresar un valor en formato número entre el 1 y el 8")
+
+    } while (isNaN(empleado)) 
+
+
+    trabajador1.tomarVacaciones(diasVacaciones)
+    trabajador4.tomarVacaciones(diasVacaciones)
+    trabajador5.tomarVacaciones(diasVacaciones)
+
+    console.log(`Se han actualizado las vacaciones de ${trabajador4.nombre}, ${trabajador1.nombre} y  ${trabajador5.nombre} que se han tomado ${diasVacaciones} días de vacaciones`)
+
+    calcularEmpleados()
+    gestionAdicional ()
+
+    }*/
+
+let otraGestion = prompt("¿Deseas realizar otra gestión? (Si o No)")
+otraGestion.toLowerCase()
+console.log(otraGestion)
+    if (otraGestion == "si") {
+    alert ("Perfecto. Continua para seleccionar el tipo de gestión que quieres realizar")
+    } else { alert ("Muchas gracias por tu tiempo, vuelve cuando quieras y que tengas un excelente día")}
+
+} while (otraGestion != "no")
